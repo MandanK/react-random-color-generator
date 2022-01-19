ce1d86839ecbc5946ea200eec5d793b1efff7959
Mandan Kazzazi <92727403+MandanK@users.noreply.github.com>
2022-01-19 20:44:46 +0100

    creating the first version of the program

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+
+function ColoredDiv(userColor) {
+  return (
+    <div
+      css={css`
+        text-align: center;
+        height: 100px;
+        font-size: 40px;
+        padding: 200px 0;
+        background-color: ${userColor.color};
+        transition: all 4s ease-out;
+        -moz-transition: all 4s ease-out;
+        -webkit-transition: all 4s ease-out;
+      `}
+    >
+      {userColor.color}
+    </div>
+  );
+}
+
+function Control() {
+  const [color, setColor] = useState('#009905');
+  const [hue, setHue] = useState('red'); // useState('green')
+  const [luminosity, setLuminosity] = useState('light'); // useState('dark')
+  const parameter = {
+    luminosity: luminosity,
+    hue: hue,
+  };
+  return (
+    <div>
+      <h1>Random Color Generator</h1>
+      <form>
+        <label>
+          <h2>Hue:</h2>
+          <input value={hue} onChange={(e) => setHue(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          <h2>Luminosity:</h2>
+          <input
+            value={luminosity}
+            onChange={(e) => setLuminosity(e.target.value)}
+          />
+        </label>
+      </form>
+      <div>
+        <button onClick={() => setColor(randomColor(parameter))}>
+          Suprise Me!
+        </button>
+        <ColoredDiv color={color} />
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Control />
       </header>
     </div>
   );
